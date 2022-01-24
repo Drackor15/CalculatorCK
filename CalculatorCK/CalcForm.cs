5ee3db5f9a2d4995a55c44350b80977b91e7a5b4
Drackor15 <connor.knapp@trojans.dsu.edu>
2022-01-24 13:49:03 -0800

    In CalcForm.cs
    Added private variables for calculations. Refined numerical buttons. Implemented btnDel_Click method under Field Buttons region. Implemented methods for btns Neg_Click, Dec_Click, & ToPercent_Click in Special Buttons region.

--- CalculatorCK/CalcForm.cs
+++ CalculatorCK/CalcForm.cs
@@ -12,6 +12,13 @@ namespace CalculatorCK
 {
     public partial class CalcForm : Form
     {
+        // Private Variables Important For Calculations
+        private Double currVal = 0;
+        //private Double sum = 0;
+
+
+
+
         public CalcForm()
         {
             InitializeComponent();
@@ -34,7 +41,10 @@ namespace CalculatorCK
             else {
                 displayInOut.Text += "0";
             }
+
+            Double.TryParse(displayInOut.Text, out currVal);
         }
+
         private void btn1_Click(object sender, EventArgs e)
         {
             if (displayInOut.Text == "0")
@@ -45,7 +55,10 @@ namespace CalculatorCK
             {
                 displayInOut.Text += "1";
             }
+
+            Double.TryParse(displayInOut.Text, out currVal);
         }
+
         private void btn2_Click(object sender, EventArgs e)
         {
             if (displayInOut.Text == "0")
@@ -56,7 +69,10 @@ namespace CalculatorCK
             {
                 displayInOut.Text += "2";
             }
+
+            Double.TryParse(displayInOut.Text, out currVal);
         }
+
         private void btn3_Click(object sender, EventArgs e)
         {
             if (displayInOut.Text == "0")
@@ -67,7 +83,10 @@ namespace CalculatorCK
             {
                 displayInOut.Text += "3";
             }
+
+            Double.TryParse(displayInOut.Text, out currVal);
         }
+
         private void btn4_Click(object sender, EventArgs e)
         {
             if (displayInOut.Text == "0")
@@ -78,7 +97,10 @@ namespace CalculatorCK
             {
                 displayInOut.Text += "4";
             }
+
+            Double.TryParse(displayInOut.Text, out currVal);
         }
+
         private void btn5_Click(object sender, EventArgs e)
         {
             if (displayInOut.Text == "0")
@@ -89,7 +111,10 @@ namespace CalculatorCK
             {
                 displayInOut.Text += "5";
             }
+
+            Double.TryParse(displayInOut.Text, out currVal);
         }
+
         private void btn6_Click(object sender, EventArgs e)
         {
             if (displayInOut.Text == "0")
@@ -100,7 +125,10 @@ namespace CalculatorCK
             {
                 displayInOut.Text += "6";
             }
+
+            Double.TryParse(displayInOut.Text, out currVal);
         }
+
         private void btn7_Click(object sender, EventArgs e)
         {
             if (displayInOut.Text == "0")
@@ -111,7 +139,10 @@ namespace CalculatorCK
             {
                 displayInOut.Text += "7";
             }
+
+            Double.TryParse(displayInOut.Text, out currVal);
         }
+
         private void btn8_Click(object sender, EventArgs e)
         {
             if (displayInOut.Text == "0")
@@ -122,7 +153,10 @@ namespace CalculatorCK
             {
                 displayInOut.Text += "8";
             }
+
+            Double.TryParse(displayInOut.Text, out currVal);
         }
+
         private void btn9_Click(object sender, EventArgs e)
         {
             if (displayInOut.Text == "0")
@@ -133,29 +167,33 @@ namespace CalculatorCK
             {
                 displayInOut.Text += "9";
             }
-        }
 
+            Double.TryParse(displayInOut.Text, out currVal);
+        }
         #endregion
 
         #region Field Buttons
         private void btnCE_Click(object sender, EventArgs e)
         {
-
+            // most likely will not touch the sum variable & will only touch currval and display
         }
 
         private void btnC_Click(object sender, EventArgs e)
         {
-
+            // most likely will touch all variables
         }
 
         private void btnDel_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void btnEquals_Click(object sender, EventArgs e)
-        {
-
+            if ((currVal < 0 && displayInOut.Text.Length == 2) || displayInOut.Text.Length == 1)
+            {
+                displayInOut.Text = "0";
+                Double.TryParse(displayInOut.Text, out currVal);
+            }
+            else {
+                displayInOut.Text = displayInOut.Text.Remove(displayInOut.Text.Length - 1, 1);
+                Double.TryParse(displayInOut.Text, out currVal);
+            }
         }
         #endregion
 
@@ -180,38 +218,56 @@ namespace CalculatorCK
 
         }
 
+        private void btnEquals_Click(object sender, EventArgs e)
+        {
+            // equals and operation buttons will operate on the sum variable
+        }
+
         private void btnSqrt_Click(object sender, EventArgs e)
         {
 
         }
 
+        private void btnXToY_Click(object sender, EventArgs e)
+        {
+
+        }
         #endregion
 
         #region Special Buttons
-
         private void btnNeg_Click(object sender, EventArgs e)
         {
-
+            if (Convert.ToDouble(displayInOut.Text) != 0) {
+                currVal *= -1;
+                displayInOut.Text = currVal.ToString();
+            }
         }
 
         private void btnDec_Click(object sender, EventArgs e)
         {
-
-        }
-        private void btnXToY_Click(object sender, EventArgs e)
-        {
-
+            if (!displayInOut.Text.Contains('.')) {
+                if (displayInOut.Text == "0")
+                {
+                    displayInOut.Text = "0.";
+                }
+                else
+                {
+                    displayInOut.Text += ".";
+                }
+            }
+            
         }
 
         private void btnInverse_Click(object sender, EventArgs e)
         {
 
         }
+
         private void btnToPercent_Click(object sender, EventArgs e)
         {
-
+            currVal /= 100;
+            displayInOut.Text = currVal.ToString();
         }
-
         #endregion
 
         #endregion
